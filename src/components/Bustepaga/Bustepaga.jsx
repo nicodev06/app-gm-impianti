103097f7f994def8ff02e85e61c0081f52de6044
Nicolò Teseo <nicolo@iMac-di-Francesco.local>
2023-03-13 17:23:29 +0100

    Buste paga

--- src/components/Bustepaga/Bustepaga.jsx
+++ src/components/Bustepaga/Bustepaga.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 import { useParams } from 'react-router-dom';
+
+import { supabase } from '../../utils/supabase-client';
+
+import { saveAs } from 'file-saver';
 
 import eye from '../../assets/eye.svg';
 
@@ -9,10 +13,90 @@ import './Bustepaga.css';
 const Bustepaga = () => {
 
   const { worker } = useParams();
-  
-  const months = ['gennaio', 'febbraio', 'marzo', 'aprile', 'maggio', 'giugno', 'luglio', 'agosto', 'settembre', 'ottobre', 'novembre', 'dicembre']
-  const availableMonths = ['gennaio', 'febbraio', 'marzo', 'aprile', 'maggio']
-  const otherMonths = months.filter((item) => !(availableMonths.includes(item)));
+  const months = ['gennaio', 'febbraio', 'marzo', 'aprile', 'maggio', 'giugno', 'luglio', 'agosto', 'settembre', 'ottobre', 'novembre', 'dicembre'];
+  const [usedMonths, setUsedMonths] = useState([]);
+  const [currentUser, setCurrentUser] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [payrolls, setPayrolls] = useState([]);
+  const [currentMonth, setCurrentMonth] = useState(null);
+  const defaultOption = useRef();
+
+  const downloadBustaPaga = (path) => {
+    supabase.storage
+    .from('main')
+    .download(path)
+    .then(({data, error}) => {
+        if (!error){
+            saveAs(data, 'busta-paga.pdf');
+        }
+    })
+  }
+
+  const uploadBustaPaga = (e) => {
+    if (currentMonth){
+        setLoading(true);
+        const file = e.target.files[0];
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+        const currentUserId = currentUser.id;
+        const path = `buste-paga/${currentUserId}+${currentMonth}+${currentYear}.pdf`;
+        supabase.storage
+        .from('main')
+        .upload(path, file)
+        .then(({data, error}) => {
+            if (!error){
+                supabase
+                .from('payrolls')
+                .insert([
+                    {
+                        user_id: currentUserId,
+                        month: currentMonth,
+                        year: currentYear,
+                        path
+                    }
+                ])
+                .then(({data, error}) => {
+                    if (!error){
+                        supabase
+                        .from('payrolls')
+                        .select('month, path')
+                        .eq('user_id', currentUser.id)
+                        .eq('year', new Date().getFullYear())
+                        .order('month', true)
+                        .then(({data, error}) => {
+                            if (!error){
+                                setPayrolls(data);
+                                setUsedMonths(data.map((item) => months[item.month]))
+                                setLoading(false);
+                                defaultOption.current.selected = 'selected';
+                            }
+                        })
+                    }
+                })
+            }
+        })
+        }
+  }
+
+  useEffect(() => {
+    supabase.auth.getUser()
+        .then(({data: {user}}) => {
+            setCurrentUser(user);
+            supabase
+            .from('payrolls')
+            .select('month, path')
+            .eq('user_id', user.id)
+            .eq('year', new Date().getFullYear())
+            .order('month', true)
+            .then(({data, error}) => {
+                if (!error){
+                    setPayrolls(data);
+                    setUsedMonths(data.map((item) => months[item.month]))
+                    setLoading(false);
+                }
+            })
+        })
+  }, [])
   
   return (
     <div className='app__buste-paga'>
@@ -20,34 +104,72 @@ const Bustepaga = () => {
             <h2 style={{textTransform: 'uppercase'}}>{worker ? "BUSTE PAGA - " + worker : "LE MIE BUSTE PAGA"}</h2>
         </div>
         <div className='app__projects'>
-            {availableMonths.map((month) => 
-                <div className='app__projects-item align-items-center'>
-                <h3 style={{ fontWeight: 300}}>{month}</h3>
-                <div className='align-items-center item-settings'>
-                    <button>
-                        <img src={eye} alt="clock" />
-                    </button>
+            {loading 
+            ? 
+            <div className='loading'>
+                <h1>CARICAMENTO...</h1>
+            </div>
+            :
+            (
+                payrolls.map(({month, path}) => 
+                    <div className='app__projects-item align-items-center'>
+                    <h3 style={{ fontWeight: 300}}>{months[month]}</h3>
+                    <div className='align-items-center item-settings'>
+                        <button onClick={() => {downloadBustaPaga(path)}}>
+                            <img src={eye} alt="clock" />
+                        </button>
+                    </div>
+                </div>
+                )
+            )
+        }
+        </div>
+        {currentUser?.user_metadata?.is_admin && (
+        <>
+            <div className='app__buste-paga-add align-items-center'>
+                <div className='attached-picture'>
+                    <label htmlFor='busta-paga-input'>+</label>
+                    <input 
+                    type='file' 
+                    accept='.pdf' 
+                    style={{display: 'none'}} 
+                    id='busta-paga-input'
+                    onChange={(e) => {uploadBustaPaga(e)}}
+                    />
+                </div>
+                <div className='app__buste-paga-choice-month'>
+                    <select 
+                    name="selezione mese..."
+                    onChange={(e) => {setCurrentMonth(e.target.value)}}
+                    >
+                        <option value="" selected ref={defaultOption}>Scegli</option>
+                        {months.filter((item) => !usedMonths.includes(item)).map((month) => 
+                        <option 
+                        value={months.indexOf(month)}
+                        >
+                            {month}
+                        </option>)}
+                    </select>
+                    <h4 style={{fontWeight: 300, marginTop: '0.5rem', padding: 0}}>carica il file in pdf</h4>
                 </div>
             </div>
-            )}
-        </div>
-        <div className='app__buste-paga-add align-items-center'>
-            <div className='attached-picture'>
-                <span>+</span>
+            <div>
+                <button className='align-items-center' style={{color: 'white', marginTop: '1rem'}}>
+                    <button className='add' style={{border: 'none', width: '1.2rem', marginRight: '1rem'}}>+</button>
+                    <label htmlFor='busta-paga-input-button'>
+                        <h3>AGGIUNGI BUSTA PAGA</h3>
+                    </label>
+                    <input 
+                    type='file' 
+                    accept='.pdf' 
+                    style={{display: 'none'}} 
+                    id='busta-paga-input-button'
+                    onChange={(e) => {uploadBustaPaga(e)}}
+                    />
+                </button>
             </div>
-            <div className='app__buste-paga-choice-month'>
-                <select name="selezione mese...">
-                    {otherMonths.map((month) => <option value={month}>{month}</option>)}
-                </select>
-                <h4 style={{fontWeight: 300, marginTop: '0.5rem', padding: 0}}>carica il file in pdf</h4>
-            </div>
-        </div>
-        <div>
-            <button className='align-items-center' style={{color: 'white', marginTop: '1rem'}}>
-                <button className='add' style={{border: 'none', width: '1.2rem', marginRight: '1rem'}}>+</button>
-                <h3>AGGIUNGI BUSTA PAGA</h3>
-            </button>
-        </div>
+        </>
+        )}
     </div>
   )
 }
